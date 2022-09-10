dbbd7813af3da56287ac841af48cb1ee3af42aab
nzerachi <melsoag@gmail.com>
2022-09-09 21:39:46 -0700

    Corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Description: a program that prints all possible combinations of single-digit numbers.
+ * Description: print all possible combinations of single-digit numbers
  * Return: Always 0 (success)
  */
 
