90db18e9c00967c0e393ed3f5a7626244b804a95
nzerachi <melsoag@gmail.com>
2022-09-13 14:16:16 -0700

    C functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: prints _putchar
+ * Return: 0 sucess
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints but in lower case
+ */
+void print_alphabet(void)
+{
+	char ch;
+	ch = a;
+	while (ch <= z)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
