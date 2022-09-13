bd277753a21ced1334612ddcc60fdc3f4ea93a61
nzerachi <melsoag@gmail.com>
2022-09-12 18:54:48 -0700

    do

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Description: Prints and that piece of art is useful without puts
- * Return: 1
+ * Return one
  */
 
 int main(void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
