0a3e91a68eea1d8d7f318240eb536de2abbc4d04
nzerachi <melsoag@gmail.com>
2022-09-13 15:50:02 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
