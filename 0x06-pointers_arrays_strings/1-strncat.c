437e8242befb75855b574b468c2a5c8b9979ab14
nzerachi <melsoag@gmail.com>
2022-09-21 20:20:34 -0700

    0x06 first correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most
- * 	an inputted number of bytes from src.
+ * _strncat - Concatenates two strings using at most an inputted no of bytes from src.
  * @dest: The string to be appended upon
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
